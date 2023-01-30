99c99c4dbf111a83defe9ef4807c9bbc942e4cbb
vyacheslavbagin@gmail.com <vyacheslavbagin@gmail.com>
2023-01-30 15:58:48 +0300

    Homework2.1 task3

--- src/Car.java
+++ src/Car.java
@@ -7,12 +7,36 @@ public class Car {
     String country;
 
     public Car(String brand, String model, double engineVolume, String color, int year, String country) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.year = year;
-        this.country = country;
+        if (brand == null || brand.isBlank()) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null || model.isBlank()) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (country == null || country.isBlank()) {
+            this.country = "default";
+        } else {
+            this.country = country;
+        }
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null || color.isBlank()) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+        if (year <= 0) {
+            this.year = 2000;
+        } else {
+            this.year = year;
+        }
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Car lada = new Car("Lada", "Granta", 1.7, "желтый", 2015, "Россия");
-        Car audi = new Car("Audi", "A8 50 L TDI quattro", 3, "черный", 2020, "Германия");
-        Car bmw = new Car("BMW", "Z8", 3, "черный", 2021, "Германия");
-        Car kia = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
-        Car hyundai = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+        Car lada = new Car("Lada", "Granta", 0, "желтый", 2015, "Россия");
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 3, "черный", 0, "Германия");
+        Car bmw = new Car("BMW", "Z8", 3, "", 2021, "Германия");
+        Car kia = new Car("Kia", "   ", 2.4, "красный", 2018, "Южная Корея");
+        Car hyundai = new Car(null, "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
 
         System.out.println(lada);
         System.out.println(audi);
