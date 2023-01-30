d8b83422c25e9461a0fa37907c55e9a5713c17c9
vyacheslavbagin@gmail.com <vyacheslavbagin@gmail.com>
2023-01-30 16:58:33 +0300

    Homework2.1 task3

--- src/Car.java
+++ src/Car.java
@@ -1,10 +1,10 @@
 public class Car {
-    String brand;
-    String model;
-    double engineVolume;
-    String color;
-    int year;
-    String country;
+    private String brand;
+    private String model;
+    private double engineVolume;
+    private String color;
+    private int year;
+    private String country;
 
     public Car(String brand, String model, double engineVolume, String color, int year, String country) {
         this.brand = brand;
@@ -12,6 +12,54 @@ public class Car {
         this.engineVolume = engineVolume;
         this.color = color;
         this.year = year;
+        this.country = country;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
         this.country = country;
     }
 
