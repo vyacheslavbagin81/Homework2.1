4ab055c4f7f195859e861831567b347432ca7705
vyacheslavbagin@gmail.com <vyacheslavbagin@gmail.com>
2023-01-30 16:51:22 +0300

    Homework2.1 task3

--- src/Car.java
+++ src/Car.java
@@ -1,10 +1,58 @@
 public class Car {
-    String brand;
-    String model;
-    double engineVolume;
-    String color;
-    int year;
-    String country;
+    private String brand;
+    private String model;
+    private double engineVolume;
+    private String color;
+    private int year;
+    private String country;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
     @Override
     public String toString() {
--- src/Main.java
+++ src/Main.java
@@ -6,45 +6,40 @@ public class Main {
         Car kia = new Car();
         Car hyundai = new Car();
 
-//        Lada Granta, 2015 год выпуска, сборка, , объем двигателя — 1,7 л.
-//                Audi A8 50 L TDI quattro, 2020 год выпуска, сборка в Германии, черный цвет кузова, объем двигателя — 3,0 л.
-//                BMW Z8, сборка в Германии в 2021 году, черный цвет кузова, объем — 3,0 л.
-//                Kia Sportage 4-го поколения, 2018 год выпуска, сборка в Южной Корее, цвет кузова — красный, объем двигателя — 2,4 л.
-//                Hyundai Avante, сборка в Южной Корее, цвет кузова — оранжевый, объем двигателя — 1,6 л, год выпуска — 2016 год
-        lada.country = "Россия";
-        lada.color = "желтый";
-        lada.brand = "Lada";
-        lada.model = "Granta";
-        lada.year = 2015;
-        lada.engineVolume = 1.7;
+        lada.setCountry("Россия");
+        lada.setColor("желтый");
+        lada.setBrand("Lada");
+        lada.setModel("Granta");
+        lada.setYear(2015);
+        lada.setEngineVolume(1.7);
 
-        audi.country = "Германия";
-        audi.color = "черный";
-        audi.brand = "Audi";
-        audi.model = "A8 50 L TDI quattro";
-        audi.year = 2020;
-        audi.engineVolume = 3;
+        audi.setCountry("Германия");
+        audi.setColor("черный");
+        audi.setBrand("Audi");
+        audi.setModel("A8 50 L TDI quattro");
+        audi.setYear(2020);
+        audi.setEngineVolume(3);
 
-        bmw.country = "Германия";
-        bmw.color = "черный";
-        bmw.brand = "BMW";
-        bmw.model = "Z8";
-        bmw.year = 2021;
-        bmw.engineVolume = 3;
+        bmw.setCountry("Германия");
+        bmw.setColor("черный");
+        bmw.setBrand("BMW");
+        bmw.setModel("Z8");
+        bmw.setYear(2021);
+        bmw.setEngineVolume(3);
 
-        kia.country = "Южная Корея";
-        kia.color = "красный";
-        kia.brand = "Kia";
-        kia.model = "Sportage 4-го поколения";
-        kia.year = 2018;
-        kia.engineVolume = 2.4;
+        kia.setCountry("Южная Корея");
+        kia.setColor("красный");
+        kia.setBrand("Kia");
+        kia.setModel("Sportage 4-го поколения");
+        kia.setYear(2018);
+        kia.setEngineVolume(2.4);
 
-        hyundai.country = "Южная Корея";
-        hyundai.color = "оранжевый";
-        hyundai.brand = "Hyundai";
-        hyundai.model = "Avante";
-        hyundai.year = 2016;
-        hyundai.engineVolume = 1.6;
+        hyundai.setCountry("Южная Корея");
+        hyundai.setColor("оранжевый");
+        hyundai.setBrand("Hyundai");
+        hyundai.setModel("Avante");
+        hyundai.setYear(2016);
+        hyundai.setEngineVolume(1.6);
 
         System.out.println(lada);
         System.out.println(audi);
