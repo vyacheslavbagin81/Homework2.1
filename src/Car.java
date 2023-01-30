1b36e7d8363069d8ee8e9be65e5a0302bdbaeee1
vyacheslavbagin@gmail.com <vyacheslavbagin@gmail.com>
2023-01-30 15:22:58 +0300

    Homework2.1 task2

--- src/Car.java
+++ src/Car.java
@@ -6,6 +6,15 @@ public class Car {
     int year;
     String country;
 
+    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
     @Override
     public String toString() {
         return "Автомобиль: " +
--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Car lada = new Car();
-        Car audi = new Car();
-        Car bmw = new Car();
-        Car kia = new Car();
-        Car hyundai = new Car();
-
-//        Lada Granta, 2015 год выпуска, сборка, , объем двигателя — 1,7 л.
-//                Audi A8 50 L TDI quattro, 2020 год выпуска, сборка в Германии, черный цвет кузова, объем двигателя — 3,0 л.
-//                BMW Z8, сборка в Германии в 2021 году, черный цвет кузова, объем — 3,0 л.
-//                Kia Sportage 4-го поколения, 2018 год выпуска, сборка в Южной Корее, цвет кузова — красный, объем двигателя — 2,4 л.
-//                Hyundai Avante, сборка в Южной Корее, цвет кузова — оранжевый, объем двигателя — 1,6 л, год выпуска — 2016 год
-        lada.country = "Россия";
-        lada.color = "желтый";
-        lada.brand = "Lada";
-        lada.model = "Granta";
-        lada.year = 2015;
-        lada.engineVolume = 1.7;
-
-        audi.country = "Германия";
-        audi.color = "черный";
-        audi.brand = "Audi";
-        audi.model = "A8 50 L TDI quattro";
-        audi.year = 2020;
-        audi.engineVolume = 3;
-
-        bmw.country = "Германия";
-        bmw.color = "черный";
-        bmw.brand = "BMW";
-        bmw.model = "Z8";
-        bmw.year = 2021;
-        bmw.engineVolume = 3;
-
-        kia.country = "Южная Корея";
-        kia.color = "красный";
-        kia.brand = "Kia";
-        kia.model = "Sportage 4-го поколения";
-        kia.year = 2018;
-        kia.engineVolume = 2.4;
-
-        hyundai.country = "Южная Корея";
-        hyundai.color = "оранжевый";
-        hyundai.brand = "Hyundai";
-        hyundai.model = "Avante";
-        hyundai.year = 2016;
-        hyundai.engineVolume = 1.6;
+        Car lada = new Car("Lada", "Granta", 1.7, "желтый", 2015, "Россия");
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 3, "черный", 2020, "Германия");
+        Car bmw = new Car("BMW", "Z8", 3, "черный", 2021, "Германия");
+        Car kia = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
+        Car hyundai = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
 
         System.out.println(lada);
         System.out.println(audi);
         System.out.println(bmw);
         System.out.println(kia);
         System.out.println(hyundai);
+
     }
 }
